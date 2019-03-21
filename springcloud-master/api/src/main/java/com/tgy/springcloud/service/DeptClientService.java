407e5ee2c765a57cc0097bb862c0098150e8fe1e
tgy616 <418982690@qq.com>
2019-03-21 15:57:53 +0800

    服务熔断，降级处理

--- springcloud-master/api/src/main/java/com/tgy/springcloud/service/DeptClientService.java
+++ springcloud-master/api/src/main/java/com/tgy/springcloud/service/DeptClientService.java
@@ -14,17 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 /**
- *
  * @Description: 修改microservicecloud-api工程，根据已经有的DeptClientService接口
-
-新建
-
-一个实现了FallbackFactory接口的类DeptClientServiceFallbackFactory
- * @author zzyy
- * @date 2018年4月21日
+ *                 新建一个实现了FallbackFactory接口的类DeptClientServiceFallbackFactory
  */
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-//@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService
 {
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
--- springcloud-master/api/src/main/java/com/tgy/springcloud/service/DeptClientServiceFallbackFactory.java
+++ springcloud-master/api/src/main/java/com/tgy/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -1,8 +1,39 @@
 package com.tgy.springcloud.service;
 
+import com.tgy.springcloud.entities.Dept;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * @author tanggy
  * @date 2019/2/20
  */
-public class DeptClientServiceFallbackFactory {
+@Component // 不要忘记添加，不要忘记添加
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService>{
+    @Override
+    public DeptClientService create(Throwable throwable)
+    {
+        return new DeptClientService() {
+            @Override
+            public Dept get(long id)
+            {
+                return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
+                        .setDb_source("no this database in MySQL");
+            }
+
+            @Override
+            public List<Dept> list()
+            {
+                return null;
+            }
+
+            @Override
+            public boolean add(Dept dept)
+            {
+                return false;
+            }
+        };
+    }
 }
