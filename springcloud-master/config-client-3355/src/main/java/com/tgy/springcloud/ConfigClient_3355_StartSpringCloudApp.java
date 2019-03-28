2657956b4cdfda4a2a3565d63413144d87ee08ec
tgy616 <418982690@qq.com>
2019-03-28 17:23:02 +0800

    代码提交#00004 config配置

---
+++ springcloud-master/config-client-3355/src/main/java/com/tgy/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package com.tgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tanggy
+ * @date 2019/3/26
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
