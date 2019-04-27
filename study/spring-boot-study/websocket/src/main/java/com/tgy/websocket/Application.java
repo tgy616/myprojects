35823a7fa1f5b65c30a4d64c20f03f5a10b936d4
tgy616 <418982690@qq.com>
2019-04-27 15:53:29 +0800

    启动类中注入enablewebsocket 、注入bean (两个类：服务端类和客户端类)

--- study/spring-boot-study/websocket/src/main/java/com/tgy/websocket/Application.java
+++ study/spring-boot-study/websocket/src/main/java/com/tgy/websocket/Application.java
@@ -2,12 +2,26 @@ package com.tgy.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
+@EnableWebSocket
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+	@Bean
+	public ChatRoomServiceEnpoint chatRoomServerEndpoint() {
+		return new ChatRoomServiceEnpoint();
 	}
 
 }
