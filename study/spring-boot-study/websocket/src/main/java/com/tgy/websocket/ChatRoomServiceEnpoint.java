375454300d90d927184654d5b6e3ea743a453ca4
tgy616 <418982690@qq.com>
2019-04-27 15:36:35 +0800

    websocket

---
+++ study/spring-boot-study/websocket/src/main/java/com/tgy/websocket/ChatRoomServiceEnpoint.java
@@ -0,0 +1,71 @@
+package com.tgy.websocket;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 聊天室 服务端
+ *
+ * @author DragonSwimDiving
+ * @program websocket.demo
+ * @Date 2019-04-27 14:34
+ **/
+@ServerEndpoint("/chat-room/{username}")
+public class ChatRoomServiceEnpoint {
+
+    private static Map<String, Session> livingSessions = new ConcurrentHashMap<String, Session>();
+
+    @OnOpen
+    public void openSession(@PathParam("username") String username, Session session) {
+
+        String sessionId = session.getId();
+
+        livingSessions.put(sessionId, session);
+
+        sendTextAll("欢迎用户[" + username + "] 来到聊天室！");
+
+    }
+
+    @OnMessage
+    public void onMessage(@PathParam("username") String username, Session session, String message) {
+
+//        sendText(session, "用户[" + username + "] : " + message);
+
+        sendTextAll("用户[" + username + "] : " + message);
+    }
+
+    private void sendTextAll(String message) {
+
+        livingSessions.forEach((sessionId, session) -> {
+            sendText(session, message);
+        });
+    }
+
+    @OnClose
+    public void onClose(@PathParam("username") String username, Session session) {
+
+        String sessionId = session.getId();
+
+        //当前的Session 移除
+        livingSessions.remove(sessionId);
+        //并且通知其他人当前用户已经离开聊天室了
+        sendTextAll("用户[" + username + "] 已经离开聊天室了！");
+    }
+
+
+    private void sendText(Session session, String message) {
+
+        RemoteEndpoint.Basic basic = session.getBasicRemote();
+
+        try {
+            basic.sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
